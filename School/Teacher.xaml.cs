7b1abcbafdbf086acbe55555c5c05ca63b695a84
Overlay404 <denis.alekseev.200411@mail.ru>
2022-10-18 16:31:23 +0300

    Заебался

--- School/Teacher.xaml.cs
+++ School/Teacher.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,35 +19,51 @@ namespace School
     /// <summary>
     /// Логика взаимодействия для Teacher.xaml
     /// </summary>
+
     public partial class Teacher : Window
     {
+        public ObservableCollection<string> Items { get; }
+        public SchoolEntities bd = new SchoolEntities();
+        public List<int> idStudent = new List<int>();
         public Teacher()
         {
-            InitializeComponent();
+            Items = new ObservableCollection<string>();
 
-            using (SchoolEntities bd = new SchoolEntities())
+            var queryStudent = (from student in bd.Student
+                                from studentLesson in student.VisitLeson
+                                from lesson in bd.LessonEmployee
+                                where lesson.IdEmployees == IdUSer.Id
+                                select student).Distinct();
+
+
+            foreach (var entity in queryStudent)
             {
-                foreach (var entity in bd.Employee.Where(employee => employee.id == IdUSer.Id))
-                    NameTeacher.Text = entity.id + " " + entity.Name
-                                       + " " + entity.Surname + "\n"
-                                       + entity.Login + " " + entity.Password;
-
-                var query = from visitLesson in bd.VisitLeson
-                            from lesson in bd.Lesson
-                            where lesson.Name == "qwe"
-                            select visitLesson;
-
-                foreach (var entity in query)
-                {
-                    ListLesson.Items.Add(entity.DateVisitLessons);
-                }
-
+                Items.Add(entity.Name + " " + entity.Surname + " " + entity.Lastname + "\n");
+                idStudent.Add(entity.id);
             }
+
+            InitializeComponent();
+            DataContext = this;
+
+            titleList.Text += DateTime.Today.ToString("d");
+
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
             new MainWindow().Show();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            for (int i = 0; i < ListLesson.SelectedItems.Count; i++)
+            {
+                VisitLeson visitLesson = new VisitLeson { id = 7, IdLesson = 1, IdStudent = idStudent.IndexOf(i), DateVisitLessons = DateTime.Today };
+                bd.VisitLeson.Add(visitLesson);
+                bd.SaveChanges();
+            }
+
         }
     }
 }
